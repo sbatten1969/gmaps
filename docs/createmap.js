fa51cafd175c545c30ba70a59e6290e4158d375c
SBatten <sbatten1969@users.noreply.github.com>
2019-05-01 16:28:45 -0400

    Add High Contrast map style, for better printing

--- docs/createmap.js
+++ docs/createmap.js
@@ -81,6 +81,105 @@ function createMapObject(createMapOptions) {
 
     var fadedMap = new google.maps.StyledMapType(stylesDeeplyFaded, {name: "Faded Map"});
 
+    var stylesHighContrast = 
+      [
+        {
+          "featureType": "landscape",
+          "elementType": "geometry",
+          "stylers": [
+            { "color": "#FFFFF0" }  // Very light yellow
+          ]
+        },{
+          "featureType": "poi",  // turn off points of interest like attractions, government buildings, etc.
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },{
+          "featureType": "poi.park",
+          "stylers": [
+            { "visibility": "on" }  // BUT do show parks
+          ]
+        },{
+          "featureType": "poi.park",
+          "elementType": "geometry.fill",
+          "stylers": [
+            { "lightness": 60 }
+          ]
+        },{
+          "featureType": "water",
+          "stylers": [
+            { "lightness": -10 }  // Set darker for higher contrast for visibility of streams/rivers
+          ]
+        },{
+          "featureType": "administrative",
+          "elementType": "labels.text",
+          "stylers": [
+            { "gamma": 1.5 }  // Fade the administrative labels like town names so they don't contrast as much
+          ]
+        },{
+          "featureType": "poi.park",
+          "elementType": "labels.text",
+          "stylers": [
+            { "gamma": 1.5 }  // Fade the park labels so they don't contrast as much
+          ]
+        },{
+          "featureType": "transit",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        },{
+          "featureType": "road",
+          "elementType": "geometry.fill",
+          "stylers": [
+            { "color": "#E0E0E0" }  // Faint grey instead of default white
+          ]
+        },{
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            { "lightness": -60 }  // Darker to look like a heavier line
+          ]
+        },{
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [
+            { "color": "#FFEEAA" }  // Moderate yellow instead of deep yellow or orange
+          ]
+        },{
+          "featureType": "road.highway",
+          "elementType": "labels.text",
+          "stylers": [
+            { "gamma": 0.01 }
+          ]
+        },{
+          "featureType": "road.arterial",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            { "lightness": -50 }  // Darker to look like a heavier line
+          ]
+        },{
+          "featureType": "road.arterial",
+          "elementType": "labels.text",
+          "stylers": [
+            { "gamma": 0.15 }
+          ]
+        },{
+          "featureType": "road.local",
+          "elementType": "geometry.stroke",
+          "stylers": [
+            { "lightness": -40 }  // Darker to look like a heavier line
+          ]
+        },{
+          "featureType": "road.local",
+          "elementType": "labels.text",
+          "stylers": [
+            { "gamma": 0.25 }
+          ]
+        }
+      ];
+
+    var highContrastMap = new google.maps.StyledMapType(stylesHighContrast, {name: "High Contrast"});
+
     var mapOptions = {
         zoom: createMapOptions.zoomLevel,
         center: new google.maps.LatLng(createMapOptions.startLatitude, createMapOptions.startLongitude),
@@ -98,7 +197,9 @@ function createMapObject(createMapOptions) {
         mapTypeControl:true,
         mapTypeControlOptions: {
           style:google.maps.MapTypeControlStyle.DROPDOWN_MENU,  
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.HYBRID, google.maps.MapTypeId.SATELLITE, 'faded_map_style', 'OSM' ],
+          mapTypeIds: [
+              google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.HYBRID, google.maps.MapTypeId.SATELLITE, 
+              'faded_map_style', 'OSM', 'highcontrast_map_style' ],
           position:google.maps.ControlPosition.TOP_RIGHT
         },
         mapTypeId: 'faded_map_style' // google.maps.MapTypeId.ROADMAP
@@ -107,6 +208,7 @@ function createMapObject(createMapOptions) {
     var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
     map.mapTypes.set('faded_map_style', fadedMap);
+    map.mapTypes.set('highcontrast_map_style', highContrastMap);
 
     // Define OSM map type pointing at the OpenStreetMap tile server
     // Source for this code snippet: http://wiki.openstreetmap.org/wiki/Google_Maps_Example
